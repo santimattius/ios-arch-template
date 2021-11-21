7adb2661a5bb3646896129437840ddc82d7e6c26
Santiago Mattiauda <santimattius@gmail.com>
2021-11-21 18:07:31 -0300

    Initial Commit

---
+++ ios-arch-template/ios_arch_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_arch_templateApp.swift
+//  ios-arch-template
+//
+//  Created by Santiago Mattiauda on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_arch_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
